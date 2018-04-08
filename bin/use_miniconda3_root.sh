e3280e75e151a9017b134a54c13e059ff9792a8d
Björn Dahlgren <bjodah@gmail.com>
2018-04-09 00:17:06 +0200

    xonsh/conda stuff

--- bin/use_miniconda3_root.sh
+++ bin/use_miniconda3_root.sh
@@ -1,16 +1,65 @@
 #!/bin/bash
 LOCATION=/opt/miniconda3
+show_help(){
+    echo "Usage:"
+    echo " -e  environment name"
+    echo " -h  show this help"
+    echo " -c  set compiler flags"
+    echo " -v  verbose"
+    echo ""
+    echo "Example:"
+    echo ""
+    echo "   \$ source $(basename $0)"
+    echo "   \$ source $(basename $0) -e sun3"
+}
+OPTIND=1
+ENV_NAME=""
+SET_FLAGS=0
+VERBOSE=0
+while getopts "h?cve:" opt; do
+    case "$opt" in
+        h|\?)
+            show_help
+            exit 0
+            ;;
+        c)
+            SET_FLAGS=1
+            ;;
+        v)
+            VERBOSE=1
+            ;;
+        e)
+            ENV_NAME=$OPTARG
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
 export PATH=$LOCATION/bin:$PATH
-echo $@
-if [ "$#" -eq 0 ]; then
+if [[ "$ENV_NAME" == "" ]]; then
     ROOT=$LOCATION
-elif [ "$#" -eq 1 ]; then
-    source activate $1
-    ROOT=$CONDA_PREFIX
 else
-    >&2 echo "Expected 0 or 1 argument"
+    source activate "$ENV_NAME"
+    ROOT=$CONDA_PREFIX
 fi
-export CPLUS_INCLUDE_PATH=$ROOT/include:$CPLUS_INCLUDE_PATH
-export C_INCLUDE_PATH=$ROOT/include:$C_INCLUDE_PATH
-export LIBRARY_PATH=$ROOT/lib:$LIBRARY_PATH
-export LD_LIBRARY_PATH=$ROOT/lib:$LD_LIBRARY_PATH
+
+if [[ $SET_FLAGS -eq 1 ]]; then
+    for DIR in lib include; do
+        if [[ ! -d "$ROOT/$DIR" ]]; then
+            >&2 echo "Not a directory: $CONDA_PREFIX/$DIR"
+            exit 1
+        fi
+    done
+    if [[ $VERBOSE -eq 1 ]]; then
+        echo "setting compiler flags..."
+    fi
+    export CPLUS_INCLUDE_PATH=$ROOT/include:$CPLUS_INCLUDE_PATH
+    export C_INCLUDE_PATH=$ROOT/include:$C_INCLUDE_PATH
+    export LIBRARY_PATH=$ROOT/lib:$LIBRARY_PATH
+    export LD_LIBRARY_PATH=$ROOT/lib:$LD_LIBRARY_PATH
+fi
+if [[ $VERBOSE -eq 1 ]]; then
+    echo "conda: $(which conda)"
+fi
