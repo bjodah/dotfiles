76434e06c6b38809082846300bfd9bb370abb85a
Björn Dahlgren <bjodah@gmail.com>
2018-07-29 21:45:11 +0200

    updates

--- bin/use_custom.sh
+++ bin/use_custom.sh
@@ -7,4 +7,10 @@ if [[ "$(hostname)" == "urania" ]]; then
     if [[ ! -e "$HOME/bin/custom/python3" ]]; then
 	ln -s "$HOME/.local/bin/python3.7" "$HOME/bin/custom/python3"
     fi
+elif [[ "$(hostname)" == "yoga720" ]]; then
+    export CC=gcc-8 CXX=g++-8 FC=gfortran-8
+    export CPATH=$HOME/.local/include:/opt/sundials-3.1.1/include:/opt/py37/include:/opt/boost_1_67_0/include
+    export LIBRARY_PATH=/opt/sundials-3.1.1/lib:/opt/py37/lib:/opt/boost_1_67_0/lib
+    export LD_LIBRARY_PATH=$LIBRARY_PATH
+    export PATH=/opt/py37/bin:$PATH
 fi
