dec60b76586d2ffbe3265e0edc127f2abdd5265d
Björn Dahlgren <bjodah@gmail.com>
2019-04-06 11:47:35 +0200

    update-yoga

--- bin/use_custom.sh
+++ bin/use_custom.sh
@@ -45,15 +45,19 @@ if [[ "$(hostname)" == "urania" ]]; then
     fi
 elif [[ "$(hostname)" == "yoga720" ]]; then
     export CC=gcc-8 CXX=g++-8 FC=gfortran-8
-    export OPENBLAS_NUM_THREADS=1
-    for PREFIX in /opt/py37 /opt/openblas-0.3.4 /opt/boost_1_68_0 /opt/symengine-6847e8a  /opt/sundials-${SUNDIALS_VERSION:-3.2.1}; do
+    export SUNDIALS_INSTALL_DIR=/opt/sundials-${SUNDIALS_VERSION:-3.2.1}
+
+    for PREFIX in /opt/py37 /opt/openblas-0.3.4 /opt/boost_1_68_0 /opt/symengine-6847e8a $SUNDIALS_INSTALL_DIR; do
         add_prefix_to_compiler_env_vars $PREFIX
     done
-    export CMAKE_PREFIX_PATH=/usr/lib/llvm-6.0:/opt/symengine-6847e8a
+    sundials_fix
+    export CMAKE_PREFIX_PATH=$SUNDIALS_INSTALL_DIR:/usr/lib/llvm-6.0:/opt/symengine-6847e8a
+
+    export OPENBLAS_NUM_THREADS=1
+
     if [[ ! -e "$HOME/bin/custom/python3.7" ]]; then
 	ln -s "/opt/py37/bin/python3.7" "$HOME/bin/custom/python3.7"
     fi
     # export PATH=/opt/py37/bin:$PATH
     export MPLBACKEND=TkAgg
-    sundials_fix
 fi
