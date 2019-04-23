4b0971efa0d651f4ce438763cee626049637097e
Björn Dahlgren <bjodah@gmail.com>
2019-04-23 15:48:58 +0200

    Added some scripts

---
+++ defaults/.bashrc.d/add-ssh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [[ "$DISPLAY" == localhost* ]] || [[ "$DISPLAY" == "" ]]; then
+    if [ -z "$SSH_AUTH_SOCK" ] ; then
+        eval `ssh-agent -s`
+        ssh-add ~/.ssh/github_id_rsa
+    fi
+fi
---
+++ defaults/.bashrc.d/bindings.sh
@@ -0,0 +1,2 @@
+bind '"\eQ":"\C-a\C-k cd ..\C-j\C-l l\C-j\C-y"'
+bind '"\eW":"\C-k\C-a cd ./\C-e\t\C-a\C-j\C-l l\C-j\C-y"'
