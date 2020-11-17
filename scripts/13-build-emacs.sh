ec0d9f830e5aef5a2c3873473154b0e801f67210
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-11-17 14:15:11 +0100

    emacs build script

--- scripts/13-build-emacs.sh
+++ scripts/13-build-emacs.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
-cd /tmp && \
-        git clone --depth 1 --branch emacs-27 git://github.com/emacs-mirror/emacs && cd emacs && ./autogen.sh && \
-        CC=gcc-10 CXX=g++-10 ./configure --with-imagemagick --with-modules  --prefix=/usr/local && \
-        make && sudo make install && cd /tmp && rm
+#!/bin/bash -e
+cd /build
+git clone --depth 1 --branch emacs-27 git://github.com/emacs-mirror/emacs
+cd emacs
+./autogen.sh
+CC=gcc-10 CXX=g++-10 ./configure --with-imagemagick --with-modules --with-json --prefix=/usr/local
+bear make -j $(nproc)
+sudo make install
+#cd /build
+#rm -r emacs
