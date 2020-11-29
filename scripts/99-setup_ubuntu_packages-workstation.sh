519b9354031ae459067f5e8d177d0ba27211f925
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-11-29 21:44:40 +0100

    add ll to bash aliases

--- scripts/99-setup_ubuntu_packages-workstation.sh
+++ scripts/99-setup_ubuntu_packages-workstation.sh
@@ -1,4 +1,4 @@
-sudo apt-get --assume-yes --no-install-recommends install nvidia-cuda-toolkit gimp htop iotop docker.io aufs-tools libsfml-dev podman
+sudo apt-get --assume-yes --no-install-recommends install nvidia-cuda-toolkit gimp htop iotop docker.io aufs-tools libsfml-dev podman python3-pyglet python3-pygame-sdl2
 
 
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
