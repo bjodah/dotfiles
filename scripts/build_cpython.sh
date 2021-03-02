c115338cd8c65de9ba8c6a4e01c364fa7c0dc618
Björn Ingvar Dahlgren <bjodah@gmail.com>
2021-03-02 08:52:33 +0100

    update script for building cpython

--- scripts/build_cpython.sh
+++ scripts/build_cpython.sh
@@ -15,14 +15,22 @@ cd "$WORKDIR"
 
 if [ -f $SRCROOT/configure ]; then
     PREFIX="/opt/cpython-$branch"
-    CC=gcc-8 CXX=g++-8 FC=gfortran-8 CFLAGS="-O2 -march=native" $SRCROOT/configure --prefix="$PREFIX" --enable-loadable-sqlite-extensions --enable-shared --with-ensurepip=yes LDFLAGS=-Wl,-rpath="$PREFIX/lib"
-    make
-    if [ -d "$PREFIX" ]; then
-        find "$PREFIX" -maxdepth 1 -mindepth 1 -exec rm -r {} \;
-    fi
-    ./python -c "import sqlite3, uuid, lzma, bz2" && make install
+    CC=${CC:-gcc-9} CXX=${CXX:-g++-9} CFLAGS="-O2 -march=native" $SRCROOT/configure \
+      --prefix="$PREFIX" \
+      --enable-loadable-sqlite-extensions \
+      --enable-shared \
+      --with-ensurepip=yes \
+      LDFLAGS=-Wl,-rpath="$PREFIX/lib"
 else
     >&2 echo "No configure script found"
+    exit 1
 fi
+bear make
+if [ -d "$PREFIX" ]; then
+    find "$PREFIX" -maxdepth 1 -mindepth 1 -exec rm -r {} \;
+fi
+LD_LIRBRARY_PATH=$(pwd) ./python -c "import sqlite3, uuid, lzma, bz2"
+make install
+ln -s $WORKDIR/compile_commands.json $PREFIX/
 "$PREFIX/bin/python3" -m pip install --upgrade --upgrade-strategy=eager pip
 PREFIX="$PREFIX" "$PREFIX"/bin/python3 -c 'import os; print("SUCCESS! Add to $PATH: %s/bin" % os.environ["PREFIX"])'
