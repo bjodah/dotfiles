16fa47f50c5e3170b8abe0438a8f2bc1ec6f2de7
Björn Ingvar Dahlgren <bjodah@gmail.com>
2021-10-05 09:19:19 +0200

    cpython-3.10

--- system-setup/19-java.sh
+++ system-setup/19-java.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -ex
-sudo apt-key adv --fetch-keys https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public
-echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ focal main" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get -y install --no-install-recommends adoptopenjdk-11-openj9
+# sudo apt-key adv --fetch-keys https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public
+# echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/ focal main" | sudo tee -a /etc/apt/sources.list
+# sudo apt-get update
+# sudo apt-get -y install --no-install-recommends adoptopenjdk-11-openj9
+
+#https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17%2B35/OpenJDK17-jdk_x64_linux_hotspot_17_35.tar.gz
+#https://github.com/adoptium/installer/issues/330#issuecomment-926987965
+
 java --version
