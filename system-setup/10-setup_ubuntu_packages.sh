e04fc842abfc4a29fa32eb19eb88bcb4dd77db66
Björn Ingvar Dahlgren <bjodah@gmail.com>
2021-11-23 11:07:24 +0100

    cm-super (for PyPI package SciencePlots)

--- system-setup/10-setup_ubuntu_packages.sh
+++ system-setup/10-setup_ubuntu_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 sudo apt-get --assume-yes --no-install-recommends install \
-     automake bat bc bear biber binutils-dev bison ca-certificates ccache cmake coinor-libipopt-dev cppcheck \
+     automake bat bc bear biber binutils-dev bison ca-certificates ccache cm-super cmake coinor-libipopt-dev cppcheck \
      curl dirmngr dot2tex doxygen dpkg-dev dvipng expect ffmpeg flex fonts-dejavu \
      fonts-humor-sans freeglut3-dev fzf g++-10 g++-10-multilib gcc-10 \
      gcc-10-multilib gcovr gdb gfortran-10 git gnupg2 gnuplot graphviz \
