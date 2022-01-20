f0ea8ae8f2952737e1af7b8f896fe38f9aee5133
Björn Ingvar Dahlgren <bjodah@gmail.com>
2022-01-20 05:08:08 +0100

    small fixes and tweaks

--- defaults/.emacs.d/init.el
+++ defaults/.emacs.d/init.el
@@ -4,12 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("fc6697788f00629cd01f4d2cc23f1994d08edb3535e4c0facef6b7247b41f5c7" "8b58ef2d23b6d164988a607ee153fd2fa35ee33efc394281b1028c2797ddeebb" default))
+   '("5e2cdea6453f8963037723ab91c779b203fb201bf5c377094440f0c465d688ec" "fc6697788f00629cd01f4d2cc23f1994d08edb3535e4c0facef6b7247b41f5c7" "8b58ef2d23b6d164988a607ee153fd2fa35ee33efc394281b1028c2797ddeebb" default))
  '(ein:jupyter-server-args '("--no-browser --allow-root"))
  '(ein:output-area-inlined-images t)
  '(flycheck-c/c++-gcc-executable "gcc-10")
  '(inhibit-startup-screen t)
- '(lsp-file-watch-threshold 2500)
  '(org-agenda-files '("~/doc/org/agendas.org"))
  '(package-selected-packages
    '(lsp-pyright ispc-mode all-the-icons tangotango-theme dakrone-theme darkburn-theme gruber-darker-theme soothe-theme moe-theme glsl-mode cuda-mode yasnippet-snippets yasnippet validate auctex jupyter ein realgud forge cython-mode lsp-mode treemacs rg use-package-ensure-system-package vterm monokai-theme monokai typescript-mode lsp-java rust-mode company flycheck lsp-ui dockerfile-mode treemacs-magit which-key dap-mode ccls realgud-lldb yaml-mode cmake-mode mmm-mode use-package))
--- defaults/.profile.d/npm.sh
+++ defaults/.profile.d/npm.sh
@@ -1,8 +1,10 @@
-NPM_PREFIX=/opt/npm-global
-if [ ! -d ${NPM_PREFIX} ]; then
-    mkdir ${NPM_PREFIX}
+if which npm; then
+    NPM_PREFIX=/opt/npm-global
+    if [ ! -d ${NPM_PREFIX} ]; then
+        mkdir ${NPM_PREFIX}
+    fi
+    if [[ "$(npm config get prefix)" != ${NPM_PREFIX} ]]; then
+        npm config set prefix ${NPM_PREFIX}
+    fi
+    export PATH="${NPM_PREFIX}/bin:$PATH"
 fi
-if [[ "$(npm config get prefix)" != ${NPM_PREFIX} ]]; then
-    npm config set prefix ${NPM_PREFIX}
-fi
-export PATH="${NPM_PREFIX}/bin:$PATH"
