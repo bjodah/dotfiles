f1d492e6de0f87280a77dd22a09c721953cab293
Björn Ingvar Dahlgren <bjodah@gmail.com>
2022-01-27 17:21:19 +0100

    smaller font-size by default in emacs

--- defaults/.emacs.d/init.el
+++ defaults/.emacs.d/init.el
@@ -24,7 +24,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 100 :width normal)))))
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 75 :width normal)))))
     (message "on MS Windows?")
 )
 
@@ -773,10 +773,3 @@
   ;;   (find-file "//ODEN/Profile\$/bjorningvar/Documents/main.org"))
   ;; (open-main-org)
 )
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 100 :width normal)))))
