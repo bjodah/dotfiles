f4056386f5e6e475238f8e6a69ea5745800f2cb6
Björn Ingvar Dahlgren <bjodah@gmail.com>
2022-03-25 23:00:18 +0100

    tweak ocaml/unison scripts

--- manual/100-build-ocaml.sh
+++ manual/100-build-ocaml.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 PREFIX=${1:-"/opt/ocaml-4.13.1"}
-mkdir -p /build
-curl -Ls https://github.com/ocaml/ocaml/archive/refs/tags/4.13.1.tar.gz | tar xz -C /build
-cd /build/ocaml-4.13.1
+BUILD_ROOT=${2:-"/build"}
+OCAML_VERSION=${3:-"4.13.1"}
+mkdir -p $BUILD_ROOT
+curl -Ls https://github.com/ocaml/ocaml/archive/refs/tags/${OCAML_VERSION}.tar.gz | tar xz -C $BUILD_ROOT
+cd ${BUILD_ROOT}/ocaml-${OCAML_VERSION}
 ./configure --prefix="$PREFIX"
 make -j $(nproc)
 make install
--- manual/110-build-unison.sh
+++ manual/110-build-unison.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-set -euxo pipefail
+set -uxo pipefail
+if ! which ocaml; then
+    >&2 echo "ocaml not on path?"
+else
+    ocaml --version
+fi
+set -e
 PREFIX=${1:-"/usr/local"}
+BUILD_ROOT=${2:-"/build"}
+UNISON_VERSION=${3:-"2.51.5"}
 mkdir -p /build
-curl -Ls https://github.com/bcpierce00/unison/archive/refs/tags/v2.51.5.tar.gz | tar xz -C /build
-cd /build/unison-2.51.5
-PATH=/opt/ocaml-4.13.1/bin:$PATH make text
+curl -Ls https://github.com/bcpierce00/unison/archive/refs/tags/v${UNISON_VERSION}.tar.gz | tar xz -C /build
+cd /build/unison-${UNISON_VERSION}
+make text
 cp ./src/unison $PREFIX/bin/
