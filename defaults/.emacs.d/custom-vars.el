3fc5fc8d3c86d960bdc4ae6e54cc51adf51d6267
Björn Ingvar Dahlgren <bjodah@gmail.com>
2022-04-05 09:32:36 +0200

    emx

---
+++ defaults/.emacs.d/custom-vars.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("5e2cdea6453f8963037723ab91c779b203fb201bf5c377094440f0c465d688ec" "fc6697788f00629cd01f4d2cc23f1994d08edb3535e4c0facef6b7247b41f5c7" "8b58ef2d23b6d164988a607ee153fd2fa35ee33efc394281b1028c2797ddeebb" default))
+ ;; '(ein:jupyter-server-args '("--no-browser --allow-root"))
+ '(ein:output-area-inlined-images t)
+ '(flycheck-c/c++-gcc-executable "gcc-10")
+ '(inhibit-startup-screen t)
+ '(org-agenda-files '("~/doc/org/agendas.org"))
+ '(package-selected-packages
+   '(idle-highlight-mode highlight-symbol evil lsp-pyright ispc-mode all-the-icons tangotango-theme dakrone-theme darkburn-theme gruber-darker-theme soothe-theme moe-theme glsl-mode cuda-mode yasnippet-snippets yasnippet validate auctex jupyter ein realgud forge cython-mode lsp-mode treemacs rg use-package-ensure-system-package vterm monokai-theme monokai typescript-mode lsp-java rust-mode company flycheck lsp-ui dockerfile-mode treemacs-magit which-key dap-mode ccls realgud-lldb yaml-mode cmake-mode mmm-mode use-package))
+ '(preview-scale-function 2)
+ '(safe-local-variable-values '((eval read-only) (org-confirm-babel-evaluate)))
+ '(vc-follow-symlinks t)
+ '(vc-git-grep-template
+   "git --no-pager grep --recurse-submodules -n <C> -e <R> -- <F>")
+ '(vterm-always-compile-module t)
+ '(vterm-shell "/usr/bin/fish"))
