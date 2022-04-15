26ff60efa0685074cb16e3d297028b125769956e
Björn Ingvar Dahlgren <bjodah@gmail.com>
2022-04-15 11:16:57 +0200

    tweak podrun

--- defaults/.emacs.d/init.el
+++ defaults/.emacs.d/init.el
@@ -14,7 +14,7 @@
      ;; If you edit it by hand, you could mess it up, so be careful.
      ;; Your init file should contain only one such instance.
      ;; If there is more than one, they won't work right.
-     '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 90 :width normal)))))
+     '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 100 :width normal)))))
   (message "on MS Windows?")
   )
 
@@ -780,9 +780,3 @@
   ;;   (find-file "//ODEN/Profile\$/bjorningvar/Documents/main.org"))
   ;; (open-main-org)
 )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 85 :width normal)))))
