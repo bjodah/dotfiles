65522e6ab60105d0efc0581dc1699e1599514598
Björn Ingvar Dahlgren <bjodah@gmail.com>
2022-04-27 23:34:34 +0200

    reorganized doom dir

--- per-file/.emacs-profiles.el
+++ per-file/.emacs-profiles.el
@@ -1,3 +1,4 @@
 (("default" . ((user-emacs-directory . "~/.emacs.default")))
- ("doom" . ((user-emacs-directory . "~/.emacs.doom")))
+ ("doom" . ((user-emacs-directory . "~/.emacs.doom")
+            (env . (("DOOMDIR" . "~/.doom.d")))))
  ("prelude" . ((user-emacs-directory . "~/.emacs.prelude"))))
