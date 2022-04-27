0b63b135833071b48cc1b7d721b1e1a0111f386f
Björn Ingvar Dahlgren <bjodah@gmail.com>
2022-04-27 17:24:42 +0200

    move early-init

--- containers/bjodahimg22heavy/env/15000-pip-install.sh
+++ containers/bjodahimg22heavy/env/15000-pip-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 # usage: pass --user flag if running on workstation, and not in docker build.
 ${PYTHON:-python3} -m pip install $@ \
-                   bokeh plotly pandas
+                   bokeh plotly pandas cppyy
--- depth-2/.emacs.d/early-init.el
+++ depth-2/.emacs.d/early-init.el
@@ -1,8 +1,8 @@
-
 (menu-bar-mode -1)
 (if (functionp 'tool-bar-mode)
     (tool-bar-mode 0))
 ;; (if window-system
 ;;     (toggle-scroll-bar -1))
-(scroll-bar-mode -1)
+(if window-system
+    (scroll-bar-mode -1))
 ;(load-theme 'tango-dark)
