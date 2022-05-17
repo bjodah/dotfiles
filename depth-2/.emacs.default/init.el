405a7cabe7c3fd8376abfd05684ae027c4c26172
Björn Ingvar Dahlgren <bjodah@gmail.com>
2022-05-17 17:50:37 +0200

    Add org-roam

--- depth-2/.emacs.default/init.el
+++ depth-2/.emacs.default/init.el
@@ -454,7 +454,21 @@
           ))
   )
 
-
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/doc/org-roam"))
+  :bind (("C-c n l" . org-roam-buffer-toogle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  (require 'org-roam-protocol)
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Global hooks and keymaps
