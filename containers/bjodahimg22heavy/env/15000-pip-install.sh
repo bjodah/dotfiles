618dc96333ead00f26467083ad01d637b9b15a5a
Björn Ingvar Dahlgren <bjodah@gmail.com>
2022-11-22 11:01:58 +0100

    updates

--- containers/bjodahimg22heavy/env/15000-pip-install.sh
+++ containers/bjodahimg22heavy/env/15000-pip-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 # usage: pass --user flag if running on workstation, and not in docker build.
 ${PYTHON:-python3} -m pip install $@ \
-                   bokeh plotly pandas cppyy xrviz numba>=0.55.1 uqpy CherryPy bottle sqlalchemy cclib
+                   bokeh plotly pandas cppyy xrviz numba>=0.55.1 uqpy CherryPy bottle sqlalchemy cclib accupy perfplots
