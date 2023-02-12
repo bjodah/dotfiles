01e619607b562609eea5551269a75e1a7e39163f
Björn Ingvar Dahlgren <bjodah@gmail.com>
2023-02-12 10:41:33 +0100

    support wayland sessions natively

--- per-leaf-dir/.profile.d/dotnet.sh
+++ per-leaf-dir/.profile.d/dotnet.sh
@@ -1,1 +1,5 @@
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
+if [ -d "$HOME/dotnet" ]; then
+    export DOTNET_CLI_TELEMETRY_OPTOUT=1
+    export DOTNET_ROOT="$HOME/dotnet"
+    export PATH="$HOME/dotnet":$PATH
+fi
