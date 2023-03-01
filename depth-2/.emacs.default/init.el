31be04dc1c9cc6d46ee325716b344cf0f2ba17b2
Björn Ingvar Dahlgren <bjodah@gmail.com>
2023-03-01 17:24:56 +0100

    updates

--- depth-2/.emacs.default/init.el
+++ depth-2/.emacs.default/init.el
@@ -132,8 +132,6 @@
                                            :debugger 'debugpy
                                            :name "Python :: Run pytest (at point)"))        
         )
-      
-
 ))
 
 ;; (use-package evil
@@ -994,9 +992,9 @@
 (global-set-key (kbd "C-c SPC") 'mark-to-space)
 
 
-(fset 'comment-c-word
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([47 kp-multiply 32 134217848 134217840 134217840 return 91 44 41 93 return 2 32 42 47] 0 "%d")) arg)))
-(global-set-key (kbd "C-c c") 'comment-c-word)
+;; (fset 'comment-c-word
+;;    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([47 kp-multiply 32 134217848 134217840 134217840 return 91 44 41 93 return 2 32 42 47] 0 "%d")) arg)))
+;; (global-set-key (kbd "C-c c") 'comment-c-word)
 
 (let ((local-settings (concat user-emacs-directory "local-settings.el")))
  (when (file-exists-p local-settings)
