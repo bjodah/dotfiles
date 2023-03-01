a42b78c694b869439d954e8dca933dd678bfdbfb
Björn Ingvar Dahlgren <bjodah@gmail.com>
2023-03-01 10:14:15 +0100

    updates

---
+++ per-leaf-dir/.bashrc.d/emacs.sh
@@ -0,0 +1,7 @@
+# git clone git://github.com/emacs-mirror/emacs && cd emacs && git checkout emacs-27 && ./autogen.sh && ./configure --with-imagemagick --with-modules --prefix=/opt/emacs-27 && make && make install
+if ! which emacs; then
+    DIR=$(ls -d /opt/emacs-*/ 2>/dev/null | head -n 1)
+    if [[ ! -z "$DIR" ]]; then
+        export PATH="${DIR%/}/bin:$PATH"
+    fi
+fi
