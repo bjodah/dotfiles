fcef05bb8cdf1f565bf23fa7f398d5b31caff82e
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-03-25 11:04:45 +0100

    fix api-keys

--- depth-2/.emacs.default/bjodah-gptel.el
+++ depth-2/.emacs.default/bjodah-gptel.el
@@ -9,7 +9,7 @@
   (setq
    gptel-model 'gemini-2.0-flash
    gptel-backend (gptel-make-gemini "Gemini"
-                   :key "GEMINI_API_KEY" ;(lambda () (shell-command-to-string "cat ~/doc/it/*nycklar*/g-gmni.* | tail -c+19 | head -c 39"))
+                   :key (lambda () (getenv "GEMINI_API_KEY")) ;(lambda () (shell-command-to-string "cat ~/doc/it/*nycklar*/g-gmni.* | tail -c+19 | head -c 39"))
                    :stream t))
   (setq gptel-temperature 0.2)
   :bind (("C-c ." . 'gptel-send)
@@ -45,7 +45,7 @@
   :host "api.groq.com"
   :endpoint "/openai/v1/chat/completions"
   :stream t
-  :key "GROQ_API_KEY" ;(lambda () (shell-command-to-string "cat ~/doc/it/*nycklar*/grq-min-nyckel-16feb.txt | tail -c+19 | head -c -6"))
+  :key (lambda () (getenv "GROQ_API_KEY")) ;(lambda () (shell-command-to-string "cat ~/doc/it/*nycklar*/grq-min-nyckel-16feb.txt | tail -c+19 | head -c -6"))
 
   :models '(;deepseek-r1-distill-llama-70b-specdec <-- use browser: https://inference.cerebras.ai/
             llama-3.3-70b-specdec
@@ -61,7 +61,7 @@
 ;; xAI offers an OpenAI compatible API
 (gptel-make-openai "xAI"           ;Any name you want
   :host "api.x.ai"
-  :key "XAI_API_KEY" ;(lambda () (shell-command-to-string "cat ~/doc/it/*nycklar*/xai-2025-feb.*"))
+  :key (lambda () (getenv "XAI_API_KEY")) ;(lambda () (shell-command-to-string "cat ~/doc/it/*nycklar*/xai-2025-feb.*"))
   :endpoint "/v1/chat/completions"
   :stream t
   :models '(;; xAI now only offers `grok-beta` as of the time of this writing
@@ -71,7 +71,7 @@
   :host "api.deepseek.com"
   :endpoint "/chat/completions"
   :stream t
-  :key "DEEPSEEK_API_KEY" ; (lambda () (shell-command-to-string "cat ~/doc/it/*nycklar*/dpsk-2025-feb.*"))
+  :key (lambda () (getenv "DEEPSEEK_API_KEY")) ; (lambda () (shell-command-to-string "cat ~/doc/it/*nycklar*/dpsk-2025-feb.*"))
   :models '(deepseek-chat ; v3
             deepseek-reasoner ; r1
             ))
