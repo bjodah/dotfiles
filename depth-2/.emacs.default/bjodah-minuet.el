73dec9fe6beaab9d2e51c912d2768e29e0e4d13a
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-03-26 15:32:56 +0100

    refactor

--- depth-2/.emacs.default/bjodah-minuet.el
+++ depth-2/.emacs.default/bjodah-minuet.el
@@ -10,6 +10,40 @@
     (minuet-set-optional-options minuet-openai-compatible-options :top_p 0.9)
     )
 
+(defun bjodah/minuet-use-localhost-fim ()
+    (setq minuet-n-completions 1) ; 1 is recommended for Local LLM for resource saving
+    ;; I recommend beginning with a small context window size and incrementally
+    ;; expanding it, depending on your local computing power. A context window
+    ;; of 512, serves as an good starting point to estimate your computing
+    ;; power. Once you have a reliable estimate of your local computing power,
+    ;; you should adjust the context window to a larger value.
+    (setq minuet-context-window 2048) ;; 512
+    (plist-put minuet-openai-fim-compatible-options :end-point "http://localhost:8000/v1/completions")
+    ;; an arbitrary non-null environment variable as placeholder
+    (plist-put minuet-openai-fim-compatible-options :name "Tabby-localhost-8000")
+    (plist-put minuet-openai-fim-compatible-options :api-key (defun my-tabby-api-key () "duck123"))
+    ;; The model is set by the llama-cpp server and cannot be altered
+    ;; post-launch.
+    (plist-put minuet-openai-fim-compatible-options :model "PLACEHOLDER")
+
+    ;; Llama.cpp does not support the `suffix` option in FIM completion.
+    ;; Therefore, we must disable it and manually populate the special
+    ;; tokens required for FIM completion.
+    (minuet-set-optional-options minuet-openai-fim-compatible-options :suffix nil :template)
+    (minuet-set-optional-options
+     minuet-openai-fim-compatible-options
+     :prompt
+     (defun minuet-llama-cpp-fim-qwen-prompt-function (ctx)
+         (format "<|fim_prefix|>%s\n%s<|fim_suffix|>%s<|fim_middle|>"
+                 (plist-get ctx :language-and-tab)
+                 (plist-get ctx :before-cursor)
+                 (plist-get ctx :after-cursor)))
+     :template)
+    (minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 256) ; or 56 for local llm?
+    (setq minuet-provider 'openai-fim-compatible)
+  )
+
+
 (defun bjodah/minuet-use-smollm2 ()
     ;; GROQ for speed:
     (plist-put minuet-openai-compatible-options :end-point "http://localhost:8001/v1/chat/completions")
@@ -47,38 +81,9 @@
     ;(add-hook 'prog-mode-hook #'minuet-auto-suggestion-mode)
 
     :config
+    (bjodah/minuet-use-localhost-fim) ; or bjodah/minuet-use-smollm2
     (setq minuet-auto-suggestion-throttle-delay 0.5)
     (setq minuet-auto-suggestion-debounce-delay 0.3)
 
     ;; You can use M-x minuet-configure-provider to interactively configure provider and model
-    (setq minuet-provider 'openai-fim-compatible)  ;; change to 'OpenAI-compatible to use GROQ/Smollm2
-    (setq minuet-n-completions 1) ; 1 is recommended for Local LLM for resource saving
-    ;; I recommend beginning with a small context window size and incrementally
-    ;; expanding it, depending on your local computing power. A context window
-    ;; of 512, serves as an good starting point to estimate your computing
-    ;; power. Once you have a reliable estimate of your local computing power,
-    ;; you should adjust the context window to a larger value.
-    (setq minuet-context-window 2048) ;; 512
-    (plist-put minuet-openai-fim-compatible-options :end-point "http://localhost:8000/v1/completions")
-    ;; an arbitrary non-null environment variable as placeholder
-    (plist-put minuet-openai-fim-compatible-options :name "Tabby-localhost-8000")
-    (plist-put minuet-openai-fim-compatible-options :api-key (defun my-tabby-api-key () "duck123"))
-    ;; The model is set by the llama-cpp server and cannot be altered
-    ;; post-launch.
-    (plist-put minuet-openai-fim-compatible-options :model "PLACEHOLDER")
-
-    ;; Llama.cpp does not support the `suffix` option in FIM completion.
-    ;; Therefore, we must disable it and manually populate the special
-    ;; tokens required for FIM completion.
-    (minuet-set-optional-options minuet-openai-fim-compatible-options :suffix nil :template)
-    (minuet-set-optional-options
-     minuet-openai-fim-compatible-options
-     :prompt
-     (defun minuet-llama-cpp-fim-qwen-prompt-function (ctx)
-         (format "<|fim_prefix|>%s\n%s<|fim_suffix|>%s<|fim_middle|>"
-                 (plist-get ctx :language-and-tab)
-                 (plist-get ctx :before-cursor)
-                 (plist-get ctx :after-cursor)))
-     :template)
-    (minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 256) ; or 56 for local llm?
     )
