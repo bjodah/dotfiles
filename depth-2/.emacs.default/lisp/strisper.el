d150c975930703fc5031f72c60eee985ca2fbf36
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-03-31 23:18:44 +0200

    gemini working over strisper

--- depth-2/.emacs.default/lisp/strisper.el
+++ depth-2/.emacs.default/lisp/strisper.el
@@ -26,38 +26,65 @@
 
 (defvar strisper--stdout-buffer-name "*strisper-stdout*")
 (defvar strisper--stderr-buffer-name "*strisper-stderr*")
-(defvar strisper--stdout-buffer-name "*strisper-processed*")
+(defvar strisper--processed-buffer-name "*strisper-processed*")
 (defvar strisper--rec-proc nil)
 
 (defun strisper--parse-line (ln)
-  ;; ln is a string of the format "<integer> <integer> <string>"
-  ;; this function discards the two integers and returns the string content.
-  ;; TODO: implement
-  )
+  "Parses a line from the server, returning just the text."
+  (let ((parts (split-string ln " " t)))
+    (if (> (length parts) 2)
+        (string-join (nthcdr 2 parts) " ")
+      "")))
+
+(defun strisper--process-buffer-line ()
+  "Processes a single line from the stdout buffer."
+  (save-excursion
+    (with-current-buffer (get-buffer strisper--stdout-buffer-name)
+      (goto-char (point-min))
+      (when (re-search-forward "^\\(.*\\)$\\n" nil t)
+        (let* ((line (match-string 1))
+               (text (strisper--parse-line line)))
+          (with-current-buffer (get-buffer-create strisper--processed-buffer-name)
+            (insert text)
+            (insert "\n"))
+          (delete-region (line-beginning-position) (line-end-position 2)))
+        ))))
 
 (defun strisper--process-arecord (&optional filterfunc)
   "Start a recording process in the background."
-  (setq strisper--rec-proc
-        (make-process
-         :name "strisper--arecord"
-         :command `("sh" "-c" "arecord -f S16_LE -c1 -r 16000 -t raw -D default | nc localhost 43007")
-         :buffer (get-buffer-create strisper--stdout-buffer-name)
-         :stderr (get-buffer-create strisper--stderr-buffer-name)
-         :coding 'utf-8
-         ))
-  ;; strisper--rec-proc now streams lines into the buffer named strisper--stdout-buffer-name
-  ;; The lines look like follows, two integers (timestamps) and then the text:
-  ;;
-  ;;    0 160  Thank  you.
-  ;;    2340 3340  Thank  you  for  watching!
+  (let ((buffer (get-buffer-create strisper--stdout-buffer-name)))
+    (set-process-filter
+     (setq strisper--rec-proc
+           (make-process
+            :name "strisper--arecord"
+            :command `("sh" "-c" "arecord -f S16_LE -c1 -r 16000 -t raw -D default | nc localhost 43007")
+            :buffer buffer
+            :stderr (get-buffer-create strisper--stderr-buffer-name)
+            :coding 'utf-8
+            :filter (lambda (process string)
+                      (with-current-buffer (process-buffer process)
+                        (insert string)
+                        (while (string-match "\\(\\n\\)" (buffer-string))
+                          (strisper--process-buffer-line)
+                          (redisplay))
+                         (when filterfunc
+                           (funcall filterfunc))))))
+     (lambda (process string)
+       (with-current-buffer (process-buffer process)
+         (insert string)
+         (redisplay)))))
 
-  ;; TODO: If filterfunc arg was provided, it is registered to process each line that is
-  ;; streamed into strisper--stdout-buffer-name
-  )
+(defun strisper-at-point-inserter ()
+  "Inserts the content of the processed buffer at point."
+  (interactive)
+  (let ((buffer (get-buffer strisper--processed-buffer-name)))
+    (when buffer
+      (save-excursion
+        (with-current-buffer buffer
+          (goto-char (point-min))
+          (insert-buffer-substring buffer (point-min) (point-max))
+          (delete-region (point-min) (point-max)))))))
 
-(defun stripser-at-point-inserter
-    ;;TODO implement)
-  
 
 (defvar strisper--recording-process nil)
 
@@ -77,7 +104,7 @@
   (if (process-live-p strisper--rec-proc)
       (when (yes-or-no-p "Already recording, kill old process?")
         (kill-process strisper--rec-proc))
-    (strisper--process-arecord strisper--at-point-inserter)))
+    (strisper--process-arecord #'strisper-at-point-inserter)))
 
 
 ;;;###autoload
@@ -91,3 +118,4 @@
 
 (provide 'strisper)
 ;;; strisper.el ends here
+
